699c877182bab573b3ae2c6f604cb12056cdd550
alyson210 <maciel.alyson@acad.ifma.edu.br>
2025-06-27 11:57:46 -0300

    Update spript.js

--- spript.js
+++ spript.js
@@ -1,29 +1,15 @@
+const btnMenu = document.getElementById('btn-menu');
+const nav = document.querySelector('.navegacao-primaria');
 
-function escrevendoLetra(){
-    function ativaLetra(elemento){
-        const arrTexto = elemento.innerHTML.split('');
-        elemento.innerHTML  = '';
-        arrTexto.forEach((letra, i)=>{
-            setTimeout(()=>{
-                elemento.innerHTML += letra;
-            }, 75 * i);
-        });
-    }
+btnMenu.addEventListener('click', () => {
+nav.classList.toggle('ativado');
+btnMenu.classList.toggle('fa-bars');
+btnMenu.classList.toggle('fa-xmark');
+});
 
-    const titulo = document.querySelector('.digitando');
-    ativaLetra(titulo);
-function ativacaoMenu() {
-    const ativaMenu = document.querySelector('.fa-bars');
-    const navMenu = document.querySelector('header .navegacao-primaria');
 
-    ativaMenu.addEventListener('click', () => {
-        ativaMenu.classList.toggle('fa-x');
-        navMenu.classList.toggle('ativado');
-    });
-}
-ativacaoMenu(); 
 
-}
+
 function sobreMim(){
     const experiencia = document.querySelectorAll('.experience_content div');
     const botao = document.querySelectorAll('.experience_content ul li')
